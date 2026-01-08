a10040bec0f2d1348dcb36936b5d8e44ea46279a
LukeRabauke <lukas.wizemann@web.de>
2026-01-08 20:08:04 +0100

    Update garmin_download.py

--- garmin_download.py
+++ garmin_download.py
@@ -6,10 +6,15 @@ class GarminDownload(hass.Hass):
 
     def initialize(self):
         self.log("GarminDownload App Initialized")
-        self.run_every(self.download_garmin_data, "now", 600)
+        #self.run_daily(self.download_garmin_data, "20:00:00")
+        self.run_every(self.download_garmin_data, "now", 60)
+
+
 
     def download_garmin_data(self, kwargs):
         self.log("Starting Garmin data download")
+        # please self.log the time of the download
+        self.log(f"Download started at {self.datetime()}")
         command = [
             "python3", "/homeassistant/appdaemon/apps/garmin-connect-export/gcexport.py",
             "-d", "/homeassistant/appdaemon/apps/MyActivities_Json",
@@ -17,8 +22,6 @@ class GarminDownload(hass.Hass):
             "--desc", "20",
             "-f", "json",
             "-u",
-            "--username", "your_username",
-            "--password", "your_password"
         ]
         try:
             result = subprocess.run(command, check=True, capture_output=True, text=True)
